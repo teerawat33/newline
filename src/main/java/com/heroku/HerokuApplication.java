112e33cf079b2fcbccc92a30b336ada95927e8bc
teerawat33 <teerawat@antinode.co.th>
2019-02-04 22:48:59 +0700

    Gradle Wrapper

--- src/main/java/com/heroku/HerokuApplication.java
+++ src/main/java/com/heroku/HerokuApplication.java
@@ -1,5 +1,10 @@
 package com.heroku;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +15,11 @@ public class HerokuApplication {
 		SpringApplication.run(HerokuApplication.class, args);
 	}
 
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("event: " + e);
+		TextMessageContent message = e.getMessage();
+		return new TextMessage(message.getText());
+	}
 }
 
